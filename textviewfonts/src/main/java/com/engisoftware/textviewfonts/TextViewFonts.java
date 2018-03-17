e2a49a47697a4ad7d03e3acc919f9ee5eb3690c2
MorochoRochaDarwin <dsmr.apps@gmail.com>
2018-03-17 13:46:30 -0500

    initial commit

---
+++ textviewfonts/src/main/java/com/engisoftware/textviewfonts/TextViewFonts.java
@@ -0,0 +1,39 @@
+package com.engisoftware.textviewfonts;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by DARWIN on 12/4/2017.
+ */
+
+@SuppressLint("AppCompatCustomView")
+public class TextViewFonts extends TextView {
+    private int pos;
+
+    public TextViewFonts(Context context) {
+        super(context);
+    }
+
+    public TextViewFonts(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TextViewFonts, 0, 0);
+        pos = a.getInt(R.styleable.TextViewFonts_dm_font, 62);
+        a.recycle();
+        init();
+    }
+
+
+    public void init() {
+
+        String[] fonts = getResources().getStringArray(R.array.fonts);
+        String font = fonts[pos];
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/" + font);
+        setTypeface(tf, 1);
+    }
+}
